417ec8fd62ce76c801fd997b6c2a2306fa5d3dd7
aZor9 <hugo.lembrez@gmail.com>
2025-04-27 17:37:11 +0200

    navbar + footer : en cours

---
+++ App/LaMaisonDuLivre/src/Controller/PartieController.php
@@ -0,0 +1,24 @@
+<?php 
+
+// src/Controller/TestController.php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController extends AbstractController
+{
+    #[Route('/navbar', name: 'navbar')]
+    public function navbar(): Response
+    {
+        return $this->render('partie/navbar.html.twig');
+    }
+
+    #[Route('/footer', name: 'footer')]
+    public function footer(): Response
+    {
+        return $this->render('partie/footer.html.twig');
+    }
+}
