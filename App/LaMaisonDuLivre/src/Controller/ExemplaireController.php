086448d873d9bb513faca355d92af2e884d1a5c3
aZor9 <hugo.lembrez@gmail.com>
2025-04-30 03:28:36 +0200

    reservation v5

--- App/LaMaisonDuLivre/src/Controller/ExemplaireController.php
+++ App/LaMaisonDuLivre/src/Controller/ExemplaireController.php
@@ -15,6 +15,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\EmpruntRepository;
+use Doctrine\ORM\EntityManagerInterface;
 
 class ExemplaireController extends AbstractController
 {
@@ -71,61 +72,24 @@ class ExemplaireController extends AbstractController
     }
     
 
-
-    // #[Route('/exemplaire/{id}', name: 'exemplaire_show')]
-    // public function show(int $id, ExemplaireRepository $exemplaireRepository, EmpruntExemplaireRepository $empruntExemplaireRepository, EmpruntRepository $empruntRepository): Response
-    // {
-    //     // Récupérer l'exemplaire spécifique
-    //     $exemplaire = $exemplaireRepository->find($id);
-        
-    //     // Récupérer les emprunts associés à cet exemplaire
-    //     $empruntsExemplaire = $empruntExemplaireRepository->findBy(['idExemplaire' => $id]);
-        
-    //     // Si l'exemplaire est réservé, on récupère l'emprunt et l'utilisateur
-    //     $reservation = null;
-    //     if (count($empruntsExemplaire) > 0) {
-    //         foreach ($empruntsExemplaire as $empruntExemplaire) {
-    //             $emprunt = $empruntRepository->find($empruntExemplaire->getIdEmprunt());
-    //             if ($emprunt && $emprunt->getDateRendu() === null) {
-    //                 $reservation = [
-    //                     'utilisateur' => $emprunt->getUtilisateur(),
-    //                     'dateReservation' => $emprunt->getDateReservation(),
-    //                     'dateRendu' => $emprunt->getDateRendu()
-    //                 ];
-    //                 break; // On prend le premier emprunt actif
-    //             }
-    //         }
-    //     }
-
-    //     return $this->render('exemplaire/show.html.twig', [
-    //         'exemplaire' => $exemplaire,
-    //         'reservation' => $reservation,
-    //     ]);
-    // }
-
-    #[Route('/exemplaire/{id}/changer-statut', name: 'exemplaire_changer_statut')]
-    public function changerStatut(int $id, EmpruntExemplaireRepository $empruntExemplaireRepository, EmpruntRepository $empruntRepository): Response
+    #[Route('/exemplaire/{id}/changer-statut', name: 'exemplaire_changer_statut', methods: ['POST'])]
+    public function changerStatut(int $id, EntityManagerInterface $entityManager): Response
     {
-        $empruntsExemplaire = $empruntExemplaireRepository->findBy(['idExemplaire' => $id]);
-        
-        foreach ($empruntsExemplaire as $empruntExemplaire) {
-            $emprunt = $empruntRepository->find($empruntExemplaire->getIdEmprunt());
-            if ($emprunt && $emprunt->getDateRendu() === null) {
-                // Changer le statut de l'exemplaire
-                if ($emprunt->getStatut() == 'reserve') {
-                    $emprunt->setStatut('utilise');
-                } elseif ($emprunt->getStatut() == 'utilise') {
-                    $emprunt->setStatut('disponible');
-                }
-                // Sauvegarder les changements
-                $entityManager = $this->getDoctrine()->getManager();
-                $entityManager->flush();
-                
-                // Rediriger vers la page des exemplaires
-                return $this->redirectToRoute('exemplaire_index');
-            }
+        $exemplaire = $entityManager->getRepository(Exemplaire::class)->find($id);
+    
+        if (!$exemplaire) {
+            throw $this->createNotFoundException('Exemplaire non trouvé.');
         }
-        
+    
+        // Changer le statut de l'exemplaire
+        if ($exemplaire->getStatut() === 'reserve') {
+            $exemplaire->setStatut('utilise');
+        } elseif ($exemplaire->getStatut() === 'utilise') {
+            $exemplaire->setStatut('disponible');
+        }
+    
+        $entityManager->flush();
+    
         return $this->redirectToRoute('exemplaire_index');
     }
     
